82c2bc963e203ec442fc9bb51e6f870afbb3986a
Pablo Pareja <pabloparejatobes@gmail.com>
2017-02-13 12:42:11 +0100

    Initial commit

---
+++ app/components/Navigation.jsx
@@ -0,0 +1,33 @@
+var React = require('react');
+var {Link, IndexLink} = require('react-router');
+
+var Navigation = React.createClass({
+
+  render: function () {
+    return(
+      <div className="top-bar">
+        <div className="top-bar-left">
+          <ul className="menu">
+            <li className="menu-text">React Time app</li>
+            <li>
+              <IndexLink to="/#" activeClassName="active" >Timer</IndexLink>
+            </li>
+            <li>
+              <Link to="/#" activeClassName="active" >Countdown</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="top-bar-right">
+          <ul className="menu">
+            <li className="menu-text">
+              Created by <a href="http://www.github.com/pablopareja">Pablo Pareja</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+
+});
+
+module.exports = Navigation;
